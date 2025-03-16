e14c4b84c3ef3c4dee3e93a723989f945f26cd72
Sekt4nt <dtoronchenko@gmail.com>
2025-03-16 17:27:29 +0000

    Precompiled headers

--- premake5.lua
+++ premake5.lua
@@ -18,6 +18,9 @@ project "Hazel-Tutor"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
+	pchheader "hzpch.h"
+	pchsource "Hazel/src/hzpch.cpp"
+
 	files
 	{
 		"%{prj.name}/src/**.h",
