44b3b2dad04764bb5654b2870d85a6481ebcc1e8
Sekt4nt <dtoronchenko@gmail.com>
2025-03-25 18:21:16 +0000

    Moveing to Sandbox

--- Hazel-Tutor/src/Hazel/Input.h
+++ Hazel-Tutor/src/Hazel/Input.h
@@ -7,7 +7,7 @@ namespace Hazel {
 	class HAZEL_API Input
 	{
 	public:
-		inline static bool IsKeyCode(int keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
+		inline static bool IsKeyPressed(int keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
 
 		inline static bool IsMouseButtonPressed(int button) { return s_Instance->IsMouseButtonPressedImpl(button); }
 		inline static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
