aaee0bf9775dac72480529ecc10163d710137831
Sekt4nt <dtoronchenko@gmail.com>
2025-03-30 12:51:28 +0100

    2D Renderer Textures

--- Hazel-Tutor/src/Hazel/Renderer/Renderer2D.h
+++ Hazel-Tutor/src/Hazel/Renderer/Renderer2D.h
@@ -2,6 +2,8 @@
 
 #include "OrthographicCamera.h"
 
+#include "Texture.h"
+
 namespace Hazel {
 
 	class Renderer2D
@@ -13,8 +15,11 @@ namespace Hazel {
 		static void BeginScene(const OrthographicCamera& camera);
 		static void EndScene();
 
+		// Primitives
 		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color);
 		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color);
+		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture);
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture);
 	};
 
 }
